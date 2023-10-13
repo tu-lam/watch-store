957bba1112bab67ae7fa15e50b24a08dd0bc8744
Tu Lam <tu.ln@outlook.com>
2023-10-13 14:27:15 +0700

    Update design

--- source/server/src/products/products.controller.ts
+++ source/server/src/products/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -8,8 +17,34 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  async create(@Body() createProductDto: CreateProductDto) {
+    if (!createProductDto.name) {
+      throw new BadRequestException({
+        messageCode: 'empty_name_product_err',
+      });
+    }
+    if (!createProductDto.slug) {
+      throw new BadRequestException({
+        messageCode: 'empty_name_product_err',
+      });
+    }
+    if (!createProductDto.price) {
+      throw new BadRequestException({
+        messageCode: 'empty_price_product_err',
+      });
+    }
+    const product = await this.productsService.create(createProductDto);
+    if (!product) {
+      return {
+        messageCode: 'add_product_fail',
+      };
+    }
+    return {
+      messageCode: 'add_product_success',
+      data: {
+        product,
+      },
+    };
   }
 
   @Get()
@@ -23,8 +58,23 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    const product = await this.productsService.update(+id, updateProductDto);
+    if (!product) {
+      return {
+        messageCode: 'edit_product_fail',
+      };
+    }
+    return {
+      messageCode: 'edit_product_success',
+      data: {
+        product,
+      },
+    };
+    // return this.productsService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
