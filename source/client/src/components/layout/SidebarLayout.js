de89cb3924e7fb53dfe185a28914de57d7d4e5eb
Tu Lam <tu.ln@outlook.com>
2023-11-03 19:56:31 +0700

    Client: Add link to logo in sidebar layout

--- source/client/src/components/layout/SidebarLayout.js
+++ source/client/src/components/layout/SidebarLayout.js
@@ -55,11 +55,13 @@ const SidebarLayout = ({
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
             <div className="flex h-16 shrink-0 items-center">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
-              />
+              <Link to={"/"}>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+              </Link>
             </div>
             <nav className="flex flex-1 flex-col">
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
