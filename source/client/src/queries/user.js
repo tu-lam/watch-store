859619ac8259fe75a05023a139866df23e52bcf7
Tu Lam <tu.ln@outlook.com>
2023-11-14 00:24:54 +0700

    Client: Fix unauthorized when get user

--- source/client/src/queries/user.js
+++ source/client/src/queries/user.js
@@ -10,10 +10,16 @@ export const getAllUsers = () => {
   });
 };
 export const getUser = async ({ queryKey }) => {
+  const token = localStorage.getItem("token");
   const res = await fetch(
     `${process.env.REACT_APP_API_URL}/users/${queryKey[1]}`,
     {
       method: "GET",
+      headers: {
+        Accept: "application.json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     }
   );
   return res.json();
